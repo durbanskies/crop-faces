0a9094f43f584d43ce2cb8bca27d2575977d77c6
durbanskies <75717651+durbanskies@users.noreply.github.com>
2020-12-09 11:55:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ from tkinter import messagebox
 import tkinter
 
 tkinter.Tk().withdraw()
-filename = askopenfilename()#select file
+filename = askopenfilename()
 
-raw_haar="facehaar.xml" #haar
-cascade=cv2.CascadeClassifier(raw_haar) #classifier
+raw_haar="facehaar.xml" #haar cascade
+cascade=cv2.CascadeClassifier(raw_haar)
 
 def croper(path):
 	image=cv2.imread(path) #read
-	grey_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) #convert to grey for faster processssing
+	grey_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) #faster
 
 	faces = cascade.detectMultiScale(grey_image,scaleFactor=1.16,minNeighbors=5,minSize=(25,25),flags=0) #img,scale,neighbors,size,flags(no)
 
 	for x,y,w,h in faces:
-		crop=image[y-10:y+h+10,x-10:x+w+10]#crop with dimensions
-		os.chdir("faces_extract")#cd faces
-		cv2.imwrite(str(randint(10000,99999))+"-face"+".jpg",crop) #save file as <randomnumber>-face.jpg
+		crop=image[y-10:y+h+10,x-10:x+w+10]#bounding
+		os.chdir("faces_extract")
+		cv2.imwrite(str(randint(10000,99999))+"-face"+".jpg",crop)
 		os.chdir("../")#cd ..
 	
-if not "faces_extract" in os.listdir("."): #check if dir present
-	os.mkdir("faces_extract")#create dir
+if not "faces_extract" in os.listdir("."): #check
+	os.mkdir("faces_extract")
 
-croper(filename)#run croper on src image input 
+croper(filename)
 messagebox.showinfo("Done","All extracted faces have been placed in faces_extract folders")
